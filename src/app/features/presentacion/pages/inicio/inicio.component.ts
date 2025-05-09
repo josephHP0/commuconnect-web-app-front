fe92f74524123321344b9a95d6c7f1d28b4c3db0
FrankDarkShadow <a20185721@pucp.edu.pe>
2025-05-08 21:20:09 -0500

    Modifico variables de entorno y assets

--- src/app/features/presentacion/pages/inicio/inicio.component.ts
+++ src/app/features/presentacion/pages/inicio/inicio.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+import { environment } from 'src/environments/environment';
+
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
   styleUrls: ['./inicio.component.css']
 })
 export class InicioComponent {
+
+  apiUrl = environment.apiUrl;
+
+  constructor() {
+    console.log('API URL cargada:', this.apiUrl);
+  }
 
 }
