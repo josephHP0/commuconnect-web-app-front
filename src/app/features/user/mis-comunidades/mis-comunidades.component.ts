22cc7b7aeaa3a4320d13303e53a9ca09475d1ea5
osorizado <plmarca@pucp.edu.pe>
2025-06-11 21:49:16 -0500

    Pantalla de resgistro completa

--- src/app/features/user/mis-comunidades/mis-comunidades.component.ts
+++ src/app/features/user/mis-comunidades/mis-comunidades.component.ts
@@ -62,4 +62,12 @@ ngOnInit(): void {
     localStorage.setItem('comunidad_seleccionada', JSON.stringify(comunidad));
      this.router.navigate(['/user/homepage', comunidad.id_comunidad]);
   }
+  cerrarSesion() {
+    // Elimina cualquier dato de sesión que hayas almacenado
+    localStorage.clear(); // O sessionStorage.clear(), dependiendo de cómo lo manejes
+    //localStorage.removeItem('token');
+
+    // Redirige a la página de presentación
+    this.router.navigate(['/presentacion/inicio']);
+  }
 }
