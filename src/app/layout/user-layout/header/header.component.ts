cc53f5884041d4a70298fb46c085866d83d783eb
osorizado <plmarca@pucp.edu.pe>
2025-06-20 17:59:01 -0500

    Mis cambios en toda la logica de la sesión y perfil

--- src/app/layout/user-layout/header/header.component.ts
+++ src/app/layout/user-layout/header/header.component.ts
@@ -1,28 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { AuthService } from 'src/app/features/autenticacion/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
-   constructor(private router: Router) {}
+  estaLogueado = false;
 
-   cerrarSesion() {
-    localStorage.removeItem('token');
+  constructor(
+    private router: Router,
+    private authService: AuthService
+  ) {}
 
-    // Verificar si realmente borró
-    const token = localStorage.getItem('token');
-    if (!token) {
-      console.log('Token eliminado correctamente, sesión cerrada.');
-    } else {
-      console.warn('Error: Token todavía existe en localStorage.');
-    }
+  ngOnInit(): void {
+    this.authService.logueado$.subscribe(estado => {
+      this.estaLogueado = estado;
+    });
 
-    this.router.navigate(['/login']);
+    this.authService.verificarToken().subscribe(valid => {
+      this.authService.setEstadoLogin(valid);
+    });
   }
 
+  cerrarSesion(): void {
+    this.authService.logout();
+    this.router.navigate(['/presentacion/inicio']);
+  }
 }
